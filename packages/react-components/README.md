bb28ab19b0b1b54e778da7367e0cf6b6b9425bfa
Jake Breindel <j.breindel2@outlook.com>
2024-04-28 08:31:25 -0400

    feat: add generated react components

---
+++ packages/react-components/README.md
@@ -0,0 +1,11 @@
+# `@pactstech/react-components`
+
+> TODO: description
+
+## Usage
+
+```
+const reactComponents = require('@pactstech/react-components');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/react-components/lib/components/stencil-generated/index.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@pactstech/stencil-components';
+
+
+
+export const ArbitrumSvg = /*@__PURE__*/createReactComponent<JSX.ArbitrumSvg, HTMLArbitrumSvgElement>('arbitrum-svg');
+export const ChainIcon = /*@__PURE__*/createReactComponent<JSX.ChainIcon, HTMLChainIconElement>('chain-icon');
+export const EthereumSvg = /*@__PURE__*/createReactComponent<JSX.EthereumSvg, HTMLEthereumSvgElement>('ethereum-svg');
+export const PactsButton = /*@__PURE__*/createReactComponent<JSX.PactsButton, HTMLPactsButtonElement>('pacts-button');
+export const TokenIcon = /*@__PURE__*/createReactComponent<JSX.TokenIcon, HTMLTokenIconElement>('token-icon');
+export const UsdcSvg = /*@__PURE__*/createReactComponent<JSX.UsdcSvg, HTMLUsdcSvgElement>('usdc-svg');
+export const UsdtSvg = /*@__PURE__*/createReactComponent<JSX.UsdtSvg, HTMLUsdtSvgElement>('usdt-svg');
+export const WbtcSvg = /*@__PURE__*/createReactComponent<JSX.WbtcSvg, HTMLWbtcSvgElement>('wbtc-svg');
